85367d0402aa0db4de43de4c2cf652d5a48b4e48
davidaburns <david_a_burns@whirlpool.com>
2021-03-28 23:07:29 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ff_ultima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
